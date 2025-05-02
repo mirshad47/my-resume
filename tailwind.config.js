d5cf27a35c8d7c39cf713b5904245a89aba17536
mirshad rahman <mirshadrahman420@gmail.com>
2025-05-02 23:37:52 +0600

    codeadd

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: 'class', // Enable dark mode using the 'class' strategy
+  content: ['./index.html'], // Specify the files Tailwind should scan for class names
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
